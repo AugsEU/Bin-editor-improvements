857900bdb41fb305e7722e5648a00a96760934d5
Augs <AugustDahlkvist@gmail.com>
2018-09-23 17:17:38 +0100

    Fixed resize issue

--- SMSSceneReader/MainForm.Designer.cs
+++ SMSSceneReader/MainForm.Designer.cs
@@ -238,7 +238,7 @@
             this.Values.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Values.ContextMenuStrip = this.paramContextMenu;
-            this.Values.Location = new System.Drawing.Point(490, 268);
+            this.Values.Location = new System.Drawing.Point(498, 268);
             this.Values.Multiline = true;
             this.Values.Name = "Values";
             this.Values.ReadOnly = true;
@@ -299,7 +299,7 @@
             this.listBox2.FormattingEnabled = true;
             this.listBox2.Location = new System.Drawing.Point(13, 280);
             this.listBox2.Name = "listBox2";
-            this.listBox2.Size = new System.Drawing.Size(471, 160);
+            this.listBox2.Size = new System.Drawing.Size(479, 160);
             this.listBox2.TabIndex = 1;
             this.listBox2.SelectedIndexChanged += new System.EventHandler(this.listBox2_SelectedIndexChanged);
             // 
@@ -333,7 +333,7 @@
             this.ParamBox.Controls.Add(this.label9);
             this.ParamBox.Controls.Add(this.paramTextBox);
             this.ParamBox.Controls.Add(this.paramLabel);
-            this.ParamBox.Location = new System.Drawing.Point(490, 376);
+            this.ParamBox.Location = new System.Drawing.Point(498, 376);
             this.ParamBox.Name = "ParamBox";
             this.ParamBox.Size = new System.Drawing.Size(250, 64);
             this.ParamBox.TabIndex = 7;
@@ -397,7 +397,7 @@
             // 
             this.label4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(486, 139);
+            this.label4.Location = new System.Drawing.Point(494, 139);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(59, 13);
             this.label4.TabIndex = 14;
@@ -412,7 +412,7 @@
             this.groupBox1.Controls.Add(this.ObjName);
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Location = new System.Drawing.Point(490, 27);
+            this.groupBox1.Location = new System.Drawing.Point(498, 27);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(250, 110);
             this.groupBox1.TabIndex = 2;
@@ -452,7 +452,7 @@
             this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(486, 252);
+            this.label5.Location = new System.Drawing.Point(494, 252);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(94, 13);
             this.label5.TabIndex = 16;
@@ -470,7 +470,7 @@
             this.aboutToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(752, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(760, 24);
             this.menuStrip1.TabIndex = 17;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -1495,9 +1495,11 @@
             // 
             // treeView1
             // 
+            this.treeView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
             this.treeView1.Location = new System.Drawing.Point(14, 43);
             this.treeView1.Name = "treeView1";
-            this.treeView1.Size = new System.Drawing.Size(470, 212);
+            this.treeView1.Size = new System.Drawing.Size(478, 212);
             this.treeView1.TabIndex = 0;
             this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
             this.treeView1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.treeView1_MouseDown);
@@ -1529,7 +1531,7 @@
             this.toolStripProgressBar1});
             this.statusStrip1.Location = new System.Drawing.Point(0, 452);
             this.statusStrip1.Name = "statusStrip1";
-            this.statusStrip1.Size = new System.Drawing.Size(752, 22);
+            this.statusStrip1.Size = new System.Drawing.Size(760, 22);
             this.statusStrip1.TabIndex = 21;
             this.statusStrip1.Text = "statusStrip1";
             // 
@@ -1549,7 +1551,7 @@
             // stringValue
             // 
             this.stringValue.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.stringValue.Location = new System.Drawing.Point(490, 155);
+            this.stringValue.Location = new System.Drawing.Point(498, 155);
             this.stringValue.Multiline = true;
             this.stringValue.Name = "stringValue";
             this.stringValue.Size = new System.Drawing.Size(251, 94);
@@ -1716,7 +1718,7 @@
             this.label8.Cursor = System.Windows.Forms.Cursors.Hand;
             this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label8.ForeColor = System.Drawing.SystemColors.HotTrack;
-            this.label8.Location = new System.Drawing.Point(580, 139);
+            this.label8.Location = new System.Drawing.Point(588, 139);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(51, 13);
             this.label8.TabIndex = 22;
@@ -1727,7 +1729,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(752, 474);
+            this.ClientSize = new System.Drawing.Size(760, 474);
             this.Controls.Add(this.label8);
             this.Controls.Add(this.stringValue);
             this.Controls.Add(this.statusStrip1);
