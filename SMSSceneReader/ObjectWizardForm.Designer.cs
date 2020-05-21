9d45cfac70dfd621b24b2f90f5a20b30c48d3685
Yoshi2 <debugyoshi@gmail.com>
2020-05-21 13:37:44 +0200

    new object wizard

---
+++ SMSSceneReader/ObjectWizardForm.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SMSSceneReader {
+    partial class ObjectWizardForm {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.button1 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.actorListBox = new System.Windows.Forms.ListBox();
+            this.objectListBox = new System.Windows.Forms.ListBox();
+            this.categoryListBox = new System.Windows.Forms.ListBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(33, 417);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(228, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Add Object";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(298, 417);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(135, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Include Manager";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // actorListBox
+            // 
+            this.actorListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.actorListBox.FormattingEnabled = true;
+            this.actorListBox.ItemHeight = 16;
+            this.actorListBox.Location = new System.Drawing.Point(15, 156);
+            this.actorListBox.Name = "actorListBox";
+            this.actorListBox.Size = new System.Drawing.Size(417, 116);
+            this.actorListBox.TabIndex = 1;
+            this.actorListBox.SelectedIndexChanged += new System.EventHandler(this.actorListBox_SelectedIndexChanged);
+            // 
+            // objectListBox
+            // 
+            this.objectListBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.objectListBox.FormattingEnabled = true;
+            this.objectListBox.ItemHeight = 16;
+            this.objectListBox.Location = new System.Drawing.Point(15, 295);
+            this.objectListBox.Name = "objectListBox";
+            this.objectListBox.Size = new System.Drawing.Size(417, 116);
+            this.objectListBox.TabIndex = 2;
+            this.objectListBox.SelectedIndexChanged += new System.EventHandler(this.listBox3_SelectedIndexChanged);
+            // 
+            // categoryListBox
+            // 
+            this.categoryListBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.categoryListBox.FormattingEnabled = true;
+            this.categoryListBox.ItemHeight = 16;
+            this.categoryListBox.Location = new System.Drawing.Point(15, 29);
+            this.categoryListBox.Name = "categoryListBox";
+            this.categoryListBox.Size = new System.Drawing.Size(417, 100);
+            this.categoryListBox.TabIndex = 0;
+            this.categoryListBox.SelectedIndexChanged += new System.EventHandler(this.categoryListBox_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 275);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Object";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(298, 444);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(121, 21);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Include Assets";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Category";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 136);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(77, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Actor Type";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // ObjectWizardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(457, 477);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.categoryListBox);
+            this.Controls.Add(this.actorListBox);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.objectListBox);
+            this.MinimumSize = new System.Drawing.Size(460, 460);
+            this.Name = "ObjectWizardForm";
+            this.Text = "Object Wizard";
+            this.Load += new System.EventHandler(this.ObjectWizardForm_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ListBox actorListBox;
+        private System.Windows.Forms.ListBox objectListBox;
+        private System.Windows.Forms.ListBox categoryListBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ SMSSceneReader/ObjectWizardForm.cs
@@ -0,0 +1,371 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using SMSReader;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SMSSceneReader {
+
+
+
+    public partial class ObjectWizardForm : Form {
+        private List<
+                (string name, 
+                 Dictionary<string, List<ObjectTemplate>> actors)
+                 > categories;
+
+        public Dictionary<string, ObjectTemplate> actorMgrs;
+        public List<ObjectTemplate> objects;
+
+        ObjectTemplate selectedObject = null;
+        string selectedCategory;
+        string selectedActor;
+        public bool addObjectClicked = false;
+        public bool includeManager;
+        public bool includeAssets;
+
+        public ObjectWizardForm() {
+            InitializeComponent();
+            includeManager = checkBox1.Checked;
+            includeAssets = checkBox2.Checked;
+
+            string basePath = System.IO.Path.GetDirectoryName(AppDomain.CurrentDomain.BaseDirectory);
+            string assetPath = Path.Combine(basePath, "GameAssets");
+            string templatePath = Path.Combine(basePath, "Templates");
+            
+            
+            Console.WriteLine(assetPath);
+            Console.WriteLine(templatePath);
+
+            categories = new List<
+                (string name, 
+                 Dictionary<string, List<ObjectTemplate>> actors)
+                 >();
+
+            actorMgrs = new Dictionary<string, ObjectTemplate>();
+            objects = new List<ObjectTemplate>();
+            
+            JsonSerializer serializer = new JsonSerializer();
+            serializer.Converters.Add(
+                    (new TupleConverter())
+                );
+
+            var dirList = Directory.GetDirectories(templatePath).ToList<string>();
+            dirList.Insert(0, templatePath);
+            dirList.Sort();
+            foreach (string dir in dirList) {
+                Console.WriteLine(dir);
+                string categoryName = new DirectoryInfo(dir).Name;
+                if (dir == templatePath) {
+                    categoryName = "No Category";
+                }
+
+                categoryListBox.Items.Add(categoryName);
+                Console.WriteLine(categoryName);
+
+                var actorDict = new Dictionary<string, List<ObjectTemplate>>();
+
+                foreach (string actorFile in Directory.GetFiles(dir)) {
+                    
+                    Console.WriteLine(actorFile);
+                    if (Path.GetExtension(actorFile).ToLower() == ".json") {
+                        using (TextReader file = File.OpenText(actorFile)) {
+                            using (JsonTextReader reader = new JsonTextReader(file)) {
+                                Console.WriteLine(actorFile);
+                                ObjectTemplate[] tempList = serializer.Deserialize<ObjectTemplate[]>(reader);
+
+                                List<ObjectTemplate> objList;
+                                foreach (ObjectTemplate temp in tempList) {
+                                    if (actorDict.TryGetValue(temp.type, out objList)) {
+                                        objList.Add(temp);
+                                    }
+                                    else {
+                                        objList = new List<ObjectTemplate>();
+                                        objList.Add(temp);
+                                        actorDict.Add(temp.type, objList);
+                                    }
+
+                                    if (!actorMgrs.ContainsKey(temp.type)) {
+                                        actorMgrs.Add(temp.type, temp);
+                                    }
+                                    objects.Add(temp);
+                                }
+                            }
+                        }
+                    }
+                }
+                categories.Add((categoryName, actorDict));
+
+            }
+        }
+
+        private void ObjectWizardForm_Load(object sender, EventArgs e) {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e) {
+            includeManager = checkBox1.Checked;
+        }
+
+
+
+        private void label1_Click(object sender, EventArgs e) {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e) {
+            includeAssets = checkBox2.Checked;
+        }
+
+        private void categoryListBox_SelectedIndexChanged(object sender, EventArgs e) {
+            int index = categoryListBox.SelectedIndex;
+            if (index < 0) {
+                return;
+            }
+            var actorDict = categories[index].actors;
+            actorListBox.Items.Clear();
+            objectListBox.Items.Clear();
+            selectedCategory = (string)categoryListBox.SelectedItem;
+            selectedObject = null;
+
+            foreach (string actor in actorDict.Keys) {
+                actorListBox.Items.Add(actor);
+            }
+        }
+
+        private void actorListBox_SelectedIndexChanged(object sender, EventArgs e) {
+            int index = categoryListBox.SelectedIndex;
+            if (index < 0) {
+                return;
+            }
+            var actorDict = categories[index].actors;
+
+            string actorName = (string)actorListBox.SelectedItem;
+            if (actorName == null) {
+                return;
+            }
+
+            selectedActor = actorName;
+            objectListBox.Items.Clear();
+            selectedObject = null;
+
+            List<ObjectTemplate> objList;
+            if (actorDict.TryGetValue(actorName, out objList)) {
+                foreach (ObjectTemplate temp in objList) {
+                    objectListBox.Items.Add(temp.name);
+                }
+            }
+        }
+
+        private void listBox3_SelectedIndexChanged(object sender, EventArgs e) {
+            int index = categoryListBox.SelectedIndex;
+            if (index < 0 | objectListBox.SelectedIndex < 0) {
+                return;
+            }
+            var actorDict = categories[index].actors;
+
+            string actorName = (string)actorListBox.SelectedItem;
+            if (actorName == null) {
+                return;
+            }
+
+            List<ObjectTemplate> objList;
+            if (actorDict.TryGetValue(actorName, out objList)) {
+                selectedObject = objList[objectListBox.SelectedIndex];
+            }
+        }
+
+        public (string category, string actor, ObjectTemplate template, bool insertMgr, bool insertAsset) backupState() {
+            return (selectedCategory, selectedActor, selectedObject, includeManager, includeAssets);
+        }
+
+        public void restoreState((string, string, ObjectTemplate, bool, bool) state) {
+            checkBox1.Checked = state.Item4;
+            checkBox2.Checked = state.Item5;
+
+            if (state.Item3 == null) {
+                return;
+            }
+
+            categoryListBox.SelectedItem = state.Item1;
+            actorListBox.SelectedItem = state.Item2;
+            objectListBox.SelectedItem = state.Item3.name;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e) {
+            if (selectedObject != null) {
+                addObjectClicked = true;
+                Close();
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e) {
+
+        }
+    }
+
+    public class ObjectTemplate {
+        public string name;
+        public string type;
+        public string objkey;
+        public string manager;
+        public (string type, object value)[] data;
+        public string[] assets;
+
+        public ObjectTemplate() {
+        
+        }
+
+        /*public ObjectTemplate(string path) {
+            JsonSerializer serializer = new JsonSerializer();
+            using (TextReader file = File.OpenText(path)) {
+                using (JsonTextReader reader = new JsonTextReader(file)) {
+                    ObjectTemplate temp = serializer.Deserialize<ObjectTemplate>(reader);
+                    
+                    name = temp.name;
+                    type = temp.type;
+                    objkey = temp.objkey;
+                    manager = temp.manager;
+                    data = temp.data;
+                    assets = temp.assets;
+                }
+            }
+
+            
+
+        }*/
+        public List<string> getManagerList() {
+            var mgrList = new List<string>();
+            if (!manager.Contains(",")) {
+                mgrList.Add(manager.Trim(' '));
+            }
+            else {
+                var mgrs = manager.Split(',');
+                foreach (string mgr in mgrs) {
+                    mgrList.Add(mgr.Trim(' '));
+                }
+            }
+
+            return mgrList;
+        }
+
+        public void CopyAssetsToStage(string sourceStash, string destinationStage) {
+            if (assets == null) {
+                return;
+            }
+
+            foreach (string filepath in assets) {
+                if (filepath == "") {
+                    continue;
+                }
+                Directory.CreateDirectory(Path.GetDirectoryName(Path.Combine(destinationStage, filepath)));
+                File.Copy(
+                    Path.Combine(sourceStash, filepath),
+                    Path.Combine(destinationStage, filepath),
+                    true
+                );
+            }
+        }
+
+        public void BackupAssetsToStash(string sourceStage, string destinationStash) {
+            if (assets == null) {
+                return;
+            }
+
+            foreach (string filepath in assets) {
+                if (filepath == "") {
+                    continue;
+                }
+                string src = Path.Combine(sourceStage, filepath);
+                string dest = Path.Combine(destinationStash, filepath);
+                if (File.Exists(src) & !Directory.Exists(dest)) {
+                    Directory.CreateDirectory(Path.GetDirectoryName(dest));
+                }
+                if (File.Exists(src)) {
+                    try {
+                        File.Copy(
+                            src,
+                            dest,
+                            true
+                        );
+                    }
+                    catch (DirectoryNotFoundException) {
+                        continue;
+                    }
+                    catch (FileNotFoundException) {
+                        continue;
+                    }
+                }
+            }
+        }
+
+        public List<string> getMissingAssets(string sourceStash) {
+            var missing = new List<string>();
+            if (assets != null) {
+                foreach (string filepath in assets) {
+                    if (filepath == "") {
+                        continue;
+                    }
+                     
+                    if (!File.Exists(Path.Combine(sourceStash, filepath))) {
+                        missing.Add(filepath);
+                    }
+                }
+            }
+
+
+            return missing;
+        }
+
+        public (GameObject obj, ObjectParameters objparams) CreateObject() {
+            ObjectParameters op = new ObjectParameters(type);
+            GameObject newObject = op.CreateEmpty();
+            op.Adjust(newObject);
+
+            newObject.SetObjectKey(objkey);
+            if (data != null) {
+                for (int i = 0; i < data.Length; i++) {
+                    op.SetParamValue(i, newObject, (string)(JValue)data[i].value);
+                }
+            }
+
+            return (newObject, op);
+        }
+
+    }
+    public class TupleConverter : JsonConverter
+    {
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
+        {
+            
+        }
+
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            var obj = JToken.Load(reader);
+            if (obj.Type == JTokenType.Array)
+            {
+                var arr = (JArray)obj;
+                if (arr.Count == 2)
+                {
+                    return new ValueTuple<string, object>((string)arr[0], arr[1]);
+                }
+            }
+            return null;
+        }
+
+        public override bool CanConvert(Type type)
+        {
+            return type.Name == "ValueTuple`2";
+        }
+    }
+}
